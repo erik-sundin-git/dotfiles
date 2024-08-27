ef90c3500cf23e05871bfc445fef801dcd4a08b9
Erik Sundin <mail@eriksundin.com>
2024-08-27 16:26:08 +0200

    nix

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -22,7 +22,7 @@
 ;; accept. For example:
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-(setq doom-font (font-spec :family "Fira Code" :size 24))
+;;(setq doom-font (font-spec :family "Fira Code" :size 24))
 
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 ;;
