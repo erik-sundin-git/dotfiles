b3e9bd345ccd2d41f8cfa05c603a6f61533eb51e
Erik Sundin <erik@thinkpad>
2026-04-15 16:42:09 +0200

    emacs

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,12 +4,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("~/notes/roam/journals/2026-04-13.org"
-     "/home/erik/notes/roam/20240912165402-agenda.org"
-     "/home/erik/notes/todo/daily.org" "/home/erik/notes/todo/inbox.org"
-     "/home/erik/notes/todo/reminders.org" "/home/erik/notes/todo/todo.org"
-     "/home/erik/notes/todo/flytt2025.org")))
+ '(safe-local-variable-values
+   '((eval add-hook 'autosync-magit-after-merge-hook #'logseq-org-roam nil t)
+     (autosync-magit-pull-timer . 600)
+     (autosync-magit-commit-message . "Update from macbook"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
