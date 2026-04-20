22ec992a09c90d75e986e10027a8d5868da579db
erik <erik@desktop>
2026-04-20 14:43:55 +0200

    nix: librewolf

--- nix/modules/browsers/librewolf/configuration.nix
+++ nix/modules/browsers/librewolf/configuration.nix
@@ -7,14 +7,28 @@
     { pkgs, config, ... }:
 
     {
+      home.sessionVariables.MOZ_USE_XINPUT2 = "1";
+
+      home.file.".librewolf/librewolf.overrides.cfg".text = ''
+        lockPref("browser.theme.content-theme", 0);
+        lockPref("browser.theme.toolbar-theme", 0);
+      '';
+
       programs.librewolf = {
         enable = true;
+
+        profiles."default".settings = {
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "browser.uidensity" = 1;
+        };
 
         profiles."default".extensions = {
           packages = with pkgs.nur.repos.rycee.firefox-addons; [
             ublock-origin
             bitwarden
             darkreader
+            sponsorblock
+            consent-o-matic
           ];
         };
       };
