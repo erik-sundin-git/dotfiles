a5cbc93e5239551ae46e252ef85d0870ae22d7b3
Erik Sundin <erik@thinkpad>
2026-04-27 16:13:47 +0200

    nix: one-dark color palette, add thermalZonePath option, add descriptions

--- nix/modules/system-constants/system-constants.nix
+++ nix/modules/system-constants/system-constants.nix
@@ -7,27 +7,39 @@
         adminEmail = lib.mkOption {
           type = lib.types.str;
           default = "mail@eriksundin.com";
+          description = "Primary email address of the admin user.";
         };
         adminName = lib.mkOption {
           type = lib.types.str;
           default = "Erik Sundin";
+          description = "Full name of the admin user.";
         };
 
         # location for use with services like redshift
         latitude = lib.mkOption {
           type = lib.types.float;
           default = 0.0;
+          description = "Geographic latitude of the host, used by redshift.";
         };
         longitude = lib.mkOption {
           type = lib.types.float;
           default = 0.0;
+          description = "Geographic longitude of the host, used by redshift.";
         };
+
+        thermalZonePath = lib.mkOption {
+          type = lib.types.nullOr lib.types.str;
+          default = null;
+          description = "Sysfs path to the thermal zone for CPU temperature display in i3status. Set to null to disable.";
+        };
+
         system = {
           type = lib.mkOption {
             type = lib.types.enum [
               "desktop"
               "laptop"
             ];
+            description = "Form factor of the host; controls laptop-specific features like touchpad and brightness keys.";
           };
           host = lib.mkOption {
             type = lib.types.enum [
@@ -35,6 +47,7 @@
               "forge"
               "ether"
             ];
+            description = "Canonical name of this host; used to select host-specific shell aliases and config.";
           };
         };
       };
