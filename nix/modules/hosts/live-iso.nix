b53844cd31837fd217b454ccfc7e026f1dec1d2e
Erik Sundin <mail@eriksundin.com>
2026-05-11 13:30:32 +0200

    nix: add live-iso config with GNOME, git, and emacs

---
+++ nix/modules/hosts/live-iso.nix
@@ -0,0 +1,41 @@
+{ inputs, ... }:
+{
+  flake.nixosConfigurations.live-iso = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix"
+      inputs.home-manager.nixosModules.home-manager
+      (
+        { pkgs, ... }:
+        {
+          nixpkgs.config.allowUnfree = true;
+          nixpkgs.overlays = [ inputs.emacs-overlay.overlays.default ];
+
+          nix.settings.experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+
+          isoImage.squashfsCompression = "lz4";
+
+          environment.systemPackages = with pkgs; [ git ];
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.nixos = {
+            imports = [ inputs.self.modules.homeManager.emacs ];
+            home.username = "nixos";
+            home.homeDirectory = "/home/nixos";
+            home.stateVersion = "25.11";
+          };
+        }
+      )
+    ];
+  };
+
+  perSystem =
+    { ... }:
+    {
+      packages.iso = inputs.self.nixosConfigurations.live-iso.config.system.build.isoImage;
+    };
+}
