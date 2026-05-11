76b5332a97d0476835e44afc4dfbc2c7a7b8ad6f
Erik Sundin <mail@eriksundin.com>
2026-05-11 17:50:11 +0200

    nixos/nomad: fill in real hardware UUIDs from nixos-generate-config

--- nix/modules/hosts/nomad/hardware.nix
+++ nix/modules/hosts/nomad/hardware.nix
@@ -15,24 +15,21 @@
 
         boot.initrd.availableKernelModules = [
           "xhci_pci"
-          "ahci"
           "nvme"
           "usb_storage"
           "sd_mod"
-          "rtsx_pci_sdmmc"
         ];
         boot.initrd.kernelModules = [ ];
         boot.kernelModules = [ "kvm-intel" ];
         boot.extraModulePackages = [ ];
 
-        # Run nixos-generate-config and replace these UUIDs
         fileSystems."/" = {
-          device = "/dev/disk/by-uuid/REPLACE-ROOT-UUID";
+          device = "/dev/disk/by-uuid/92368933-ab52-4dd9-a201-64684566f2ce";
           fsType = "ext4";
         };
 
         fileSystems."/boot" = {
-          device = "/dev/disk/by-uuid/REPLACE-BOOT-UUID";
+          device = "/dev/disk/by-uuid/8EA7-FD95";
           fsType = "vfat";
           options = [
             "fmask=0077"
@@ -41,7 +38,7 @@
         };
 
         swapDevices = [
-          { device = "/dev/disk/by-uuid/REPLACE-SWAP-UUID"; }
+          { device = "/dev/disk/by-uuid/043a7934-4f9b-469c-9b93-459f190449d8"; }
         ];
 
         nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
