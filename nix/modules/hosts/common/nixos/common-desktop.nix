318e088c73068e58dd56d7e27aeac705adc11b2a
Erik Sundin <mail@eriksundin.com>
2026-05-11 19:18:01 +0200

    common-desktop: improve bluetooth audio quality via pipewire/bluez config
    Enable SBC-XQ, mSBC, hardware volume, and BlueZ experimental mode for
    LDAC support; reduces unwanted HSP/HFP profile switching.

--- nix/modules/hosts/common/nixos/common-desktop.nix
+++ nix/modules/hosts/common/nixos/common-desktop.nix
@@ -51,6 +51,16 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
+        wireplumber.extraConfig."10-bluez" = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.auto-connect" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+          };
+        };
       };
+
+      hardware.bluetooth.settings.General.Experimental = true;
     };
 }
